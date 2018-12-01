0b701d5b65c8f1d475082c2ac65848e92f742c73
Evgen097 <k0990245853@gmail.com>
2018-12-01 10:47:05 +0200

    first commit

---
+++ 2.js
@@ -0,0 +1,181 @@
+// …or create a new repository on the command line
+// echo "# tankgame" >> README.md
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git remote add origin https://github.com/Evgen097/tankgame.git
+//     git push -u origin master
+// …or push an existing repository from the command line
+// git remote add origin https://github.com/Evgen097/tankgame.git
+//     git push -u origin master
+
+
+
+var barrel = document.querySelector('#barrel');
+var tankshell = document.querySelector('.tankshell');
+var breechsight = document.querySelector('#breechsight');
+var tower = document.querySelector('#tower');
+
+// tower.style.transform = 'rotate(0deg)';
+// tankshell.style.transform = 'rotate(0deg)';
+// setInterval( ()=>{
+//     var rotate = parseInt(tower.style.transform.replace(/[^\d.]/g, ''))+10;
+//     tower.style.transform = `rotate(${rotate}deg)`;
+//     tankshell.style.transform = `rotate(${rotate}deg)`;
+//     console.log(barrel.style.transform)
+//
+//     breechsightX = breechsight.getBoundingClientRect().left;
+//     breechsightY = breechsight.getBoundingClientRect().top;
+//
+//     tankshellX = tankshell.getBoundingClientRect().left;
+//     tankshellY = tankshell.getBoundingClientRect().top;
+//
+//     vectorX = (breechsightX - tankshellX)/10;
+//     console.log('vectorX ', vectorX);
+//
+//     vectorY = (breechsightY - tankshellY)/10;
+//     console.log('vectorY ', vectorY);
+//
+//
+// }, 3000 )
+
+// var breechsightX = breechsight.getBoundingClientRect().left;
+// var breechsightY = breechsight.getBoundingClientRect().top;
+//
+// console.log('breechsightX ', breechsightX);
+// console.log('breechsightY ', breechsightY);
+//
+// var tankshellX = tankshell.getBoundingClientRect().left;
+// var tankshellY = tankshell.getBoundingClientRect().top;
+// console.log('tankshellX ', tankshellX);
+// console.log('tankshellY ', tankshellY);
+//
+// var x = parseFloat(getComputedStyle(tankshell).left);
+// var y = parseFloat(getComputedStyle(tankshell).top);
+// console.log('x ', x)
+// console.log('y ',y)
+//
+//
+//
+// var vectorX = (breechsightX - tankshellX)/10;
+// console.log('vectorX ', vectorX);
+//
+// var vectorY = (breechsightY - tankshellY)/10;
+// console.log('vectorY ', vectorY);
+
+// setInterval( ()=>{
+//     var x = parseFloat(getComputedStyle(tankshell).left);
+//     var y = parseFloat(getComputedStyle(tankshell).top);
+//
+//     tankshell.style.left = (x + vectorX) + 'px';
+//     tankshell.style.top = (y + vectorY) + 'px';
+//
+//     console.log(x)
+//     console.log(y)
+// }, 20 )
+//
+// setInterval( ()=>{
+//     tankshell.style.left = 22 + 'px';
+//     tankshell.style.top = 22 + 'px';
+// }, 1000 )
+
+
+
+
+
+
+// var breechsightX = breechsight.getBoundingClientRect().left;
+// var breechsightY = breechsight.getBoundingClientRect().top;
+//
+// console.log('breechsightX ', breechsightX);
+// console.log('breechsightY ', breechsightY);
+//
+// var tankshellX = tankshell.getBoundingClientRect().left;
+// var tankshellY = tankshell.getBoundingClientRect().top;
+// console.log('tankshellX ', tankshellX);
+// console.log('tankshellY ', tankshellY);
+//
+// var x = parseFloat(getComputedStyle(tankshell).left);
+// var y = parseFloat(getComputedStyle(tankshell).top);
+// console.log('x ', x)
+// console.log('y ',y)
+//
+//
+//
+// var vectorX = (breechsightX - tankshellX)/10;
+// console.log('vectorX ', vectorX);
+//
+// var vectorY = (breechsightY - tankshellY)/10;
+// console.log('vectorY ', vectorY);
+
+// var x = parseFloat(getComputedStyle(tankshell).left);
+// var y = parseFloat(getComputedStyle(tankshell).top);
+// tankshell.style.left = (x + vectorX) + 'px';
+// tankshell.style.top = (y + vectorY) + 'px';
+
+
+// setInterval( ()=>{
+//     var x = parseFloat(getComputedStyle(tankshell).left);
+//     var y = parseFloat(getComputedStyle(tankshell).top);
+//     tankshell.style.left = (x + vectorX) + 'px';
+//     tankshell.style.top = (y + vectorY) + 'px';
+//     console.log(x)
+//     console.log(y)
+// }, 20 )
+
+// setInterval( ()=>{
+//     tankshell.style.left = 17 + 'px';
+//     tankshell.style.top = 17 + 'px';
+//     var rotate = parseInt(barrel.style.transform.replace(/[^\d.]/g, ''))+10;
+//     barrel.style.transform = `rotate(${rotate}deg)`;
+//     tankshell.style.transform = `rotate(${rotate}deg)`;
+//
+//     breechsightX = breechsight.getBoundingClientRect().left;
+//     breechsightY = breechsight.getBoundingClientRect().top;
+//
+//
+//     tankshellX = tankshell.getBoundingClientRect().left;
+//     tankshellY = tankshell.getBoundingClientRect().top;
+//
+//
+//     x = parseFloat(getComputedStyle(tankshell).left);
+//     y = parseFloat(getComputedStyle(tankshell).top);
+//
+//
+//     vectorX = (breechsightX - tankshellX)/10;
+//     vectorY = (breechsightY - tankshellY)/10;
+//
+//
+// }, 1000 )
+
+
+
+
+// setInterval( ()=>{
+// var x = parseInt(getComputedStyle(tankshell).left);
+// var y = parseInt(getComputedStyle(tankshell).top);
+// tankshell.style.left = (x+10) + 'px';
+// tankshell.style.top = (y+10) + 'px';
+// console.log(x)
+// console.log(y)
+// }, 1000 )
+
+
+
+
+
+
+
+
+
+
+// var barrel = document.querySelector('#barrel');
+// barrel.style.transform = 'rotate(0deg)';
+// tankshell.style.transform = 'rotate(0deg)';
+// setInterval( ()=>{
+//     var rotate = parseInt(barrel.style.transform.replace(/[^\d.]/g, ''))+10;
+//     barrel.style.transform = `rotate(${rotate}deg)`;
+//     tankshell.style.transform = `rotate(${rotate}deg)`;
+//     console.log(barrel.style.transform)
+//
+// }, 3000 )
